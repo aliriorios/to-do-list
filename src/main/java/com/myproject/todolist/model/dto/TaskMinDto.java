4eb5bff0a967f800834b8064426f18000a6e9cb3
Alírio de Souza Rios <alirioriosmc@gmail.com>
2023-07-24 13:56:10 -0300

    Criando TaskMinDto

---
+++ src/main/java/com/myproject/todolist/model/dto/TaskMinDto.java
@@ -0,0 +1,32 @@
+package com.myproject.todolist.model.dto;
+
+import com.myproject.todolist.model.entity.Task;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class TaskMinDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String title;
+    private LocalDate delivery;
+    private Integer taskStatus;
+    
+    //constructor
+    public TaskMinDto (Task task) {
+        BeanUtils.copyProperties(task, this);
+    }
+}
