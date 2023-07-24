3c389aca852ae2274c18d097678fc18000e873bf
Alírio de Souza Rios <alirioriosmc@gmail.com>
2023-07-24 17:26:44 -0300

    Implementando requisições.
    CRUD das tarefas.
    Requisição de update está com erro para ser corrigido!

--- src/main/java/com/myproject/todolist/controller/TaskController.java
+++ src/main/java/com/myproject/todolist/controller/TaskController.java
@@ -2,8 +2,10 @@ package com.myproject.todolist.controller;
 
 import com.myproject.todolist.model.dto.TaskDto;
 import com.myproject.todolist.model.dto.TaskMinDto;
+import com.myproject.todolist.model.entity.Task;
 import com.myproject.todolist.model.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +18,29 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    public ResponseEntity<List<TaskMinDto>> listAllTasks() {
-        return ResponseEntity.ok().body(taskService.listAllTasks());
+    public ResponseEntity<List<TaskMinDto>> listAll() {
+        return ResponseEntity.ok().body(taskService.listAll());
     }
 
-    @GetMapping("/showTask/{id}")
-    public ResponseEntity<TaskDto> showTask(@PathVariable Long id) {
-        return ResponseEntity.ok().body(taskService.showTask(id));
+    @GetMapping("/showById/{id}")
+    public ResponseEntity<TaskDto> showById (@PathVariable Long id) {
+        return ResponseEntity.ok().body(taskService.showById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> save (@RequestBody Task task) {
+        return new ResponseEntity<>(taskService.save(task), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update (@PathVariable Long id, @RequestBody Task task) {
+        task = taskService.update(id, task);
+        return ResponseEntity.ok().body(task);
     }
 }
--- src/main/java/com/myproject/todolist/model/service/TaskService.java
+++ src/main/java/com/myproject/todolist/model/service/TaskService.java
@@ -4,7 +4,12 @@ import com.myproject.todolist.model.dto.TaskDto;
 import com.myproject.todolist.model.dto.TaskMinDto;
 import com.myproject.todolist.model.entity.Task;
 import com.myproject.todolist.model.repository.TaskRepository;
+import com.myproject.todolist.model.service.exception.DatabaseException;
+import com.myproject.todolist.model.service.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +22,50 @@ public class TaskService {
     private TaskRepository taskRepository;
 
     @Transactional(readOnly = true)
-    public List<TaskMinDto> listAllTasks() {
+    public List<TaskMinDto> listAll() {
         List<Task> result = taskRepository.findAll();
         return result.stream().map(TaskMinDto::new).toList();
     }
 
     @Transactional(readOnly = true)
-    public TaskDto showTask (Long id){
+    public TaskDto showById (Long id){
         Optional<Task> result = taskRepository.findById(id);
         return new TaskDto(result.get());
+    }
+
+    public Task save (Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void delete (Long id) {
+        try {
+            taskRepository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    /* ERRO XXXXXXXXXXXXXXXXXXXXXXXXX*/
+    public Task update (Long id, Task task) {
+        try {
+            Task entity = taskRepository.getReferenceById(id); //get the reference of id entity
+            updateData(entity, task); //update 'entity' with values of 'task'
+            return taskRepository.save(entity);
+
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    /* PRIVATE -------------------------------------------------------------------------- */
+    private void updateData (Task entity, Task task) {
+        entity.setTitle(task.getTitle());
+        entity.setDescription(task.getDescription());
+        entity.setDelivery(task.getDelivery());
+        entity.setTaskStatus(task.getTaskStatus());
     }
 }
