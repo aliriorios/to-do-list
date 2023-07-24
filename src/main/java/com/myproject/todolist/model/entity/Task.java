44441c88410c9ff347ef020ba3451987dcda0fec
Alírio de Souza Rios <alirioriosmc@gmail.com>
2023-07-24 11:03:39 -0300

    Refatorando estrutura do projeto.
    Refazendo a estrutura de camadas  MVC do projeto.
    Juntando as pastas e colocando o front na camada "view".

---
+++ src/main/java/com/myproject/todolist/model/entity/Task.java
@@ -0,0 +1,32 @@
+package com.myproject.todolist.model.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+public class Task implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate delivery;
+}
